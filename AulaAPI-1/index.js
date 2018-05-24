2feff949bd6b2555a6b29bb246f29c5a6ba16d9e
alinesilva02 <37853344+alinesilva02@users.noreply.github.com>
2018-05-24 08:58:40 -0300

    commit ok

--- AulaAPI-1/index.js
+++ AulaAPI-1/index.js
@@ -4,6 +4,8 @@ const chalk = require('chalk');
 
 const app = express();
 
+let db = [];
+
 
 app.get(`/cep/:cepinput`, (request, response) => {
     const cep = request.params.cepinput;
@@ -31,12 +33,12 @@ app.get(`/cep/:cepinput/rua`, (request, response) => {
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', resposta && resposta.statusCode); // Print the response status code if a response was received
         console.log('body:', body); // Print the HTML for the Google homepage.
-        
+
         //primeiro coloquei o body em formato json
         const rua = JSON.parse(body);
         //peguei somente o campo logadorouro que esta no body
         const logradouro = rua.logradouro;
-        
+
         response.status(200).send(logradouro);
 
     });
@@ -49,7 +51,7 @@ app.get(`/cep/:cepinput/bairro`, (request, response) => {
     const url = `https://viacep.com.br/ws/${cep}/json`;
 
     httprequest(url, function (error, resposta, body) {
-        
+
         console.log('error:', error); // Print the error if one occurred
         console.log('statusCode:', resposta && resposta.statusCode); // Print the response status code if a response was received
         console.log('body:', body); // Print the HTML for the Google homepage.
@@ -64,6 +66,28 @@ app.get(`/cep/:cepinput/bairro`, (request, response) => {
 
 });
 
+
+//tem o objtivo para criar um usuário post (qdo envia algo para o front end)
+app.post('/usuario', (request, response) => {
+    console.log('POST /usuario');
+
+
+    let user = {
+        nome: request.body.nome,
+        sobrenome: request.body.sobrenome,
+        cep: request.body.cep,
+        numero: request.body.numero,
+        complemento: request.body.complemento,
+        telefone: request.body.telefone,
+        senha: request.body.senha
+    };
+    if (user.nome == '') {
+        response.status(400).send('nome incorre, por favor preencha novamente');
+    }
+     
+    db.push(user);
+    response.status(201).send ('status');
+});
 
 
 app.listen(3000, () => {
